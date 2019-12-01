d6e599933bd025047ca6b39ec259dbb3d7a6f825
khantkyawkhaung <khantkyawkhaung288@gmail.com>
2019-12-01 23:30:00 +0630

    Wrong HTML file

--- footer.js
+++ footer.js
@@ -1,6 +1,6 @@
 // DOM manipulation after the content is loaded
 document.addEventListener('DOMContentLoaded', async function() {
     let footer = document.getElementById("footer");
-    const response = await fetch("https://staytu.github.io/header.html");
+    const response = await fetch("https://staytu.github.io/footer.html");
     footer.innerHTML = await response.text();
 }, false);
